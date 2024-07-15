09e03a42dd7991db3c428c5426720bc7e39d57e8
nirmal-nivin <nirmalbkumar95@gmail.com>
2024-07-15 14:04:16 +0530

    updated contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,35 @@
 import React from "react";
+import Swal from 'sweetalert2'
 
 const Contact = () =>{
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "36d23fd9-40cc-451a-a395-86dd0dc70c19");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+            Swal.fire({
+                title: "Success",
+                text: "Message sent successfully!",
+                icon: "success"
+              });
+        }
+      };
+
     return(
         <div name='Contact' className="w-full h-screen bg-gradient-to-b from-indigo-950 to-indigo-950 p-4 text-white">
             <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
@@ -10,7 +39,7 @@ const Contact = () =>{
                 </div>
 
                 <div className="flex justify-center items-center">
-                     <form action="#" className="flex flex-col w-full md:w-1/2">
+                     <form onSubmit={onSubmit} action="#" className="flex flex-col w-full md:w-1/2">
 
                         <input type="text"
                         name="name"
@@ -31,7 +60,7 @@ const Contact = () =>{
                          className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none">
                          </textarea>
 
-                         <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 
+                         <button type="submit" className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 
                             px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
                                 Lets's Talk
                         </button>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,47 @@
 import React from "react";
 import { MdArrowRightAlt } from "react-icons/md";
-// import hero from "../assets/hero.png"
+import hero from "../assets/hero.png";
 import { Link } from "react-scroll";
 
 const Home = () => {
   return (
     <div
       name="Home"
-      className="h-screen w-full bg-gradient-to-b from-indigo-950 to-indigo-950">
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
-          <p className="text-2xl sm:text-4xl font-bold text-white">Hi, I am</p>
-          <h2 className="text-white font-bold text-2xl sm:text-4xl mt-2">
-            Nirmal kumar
+      className="h-screen w-full bg-gradient-to-b from-indigo-950 to-indigo-950 flex items-center">
+      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-between h-full px-4 ">
+        <div className="flex flex-col justify-center h-full text-center md:text-left">
+          <p className="text-2xl sm:text-3xl lg:text-4xl font-bold text-white">Hi, I am</p>
+          <h2 className="text-white font-bold text-3xl sm:text-4xl lg:text-5xl mt-2">
+            Nirmal Kumar
           </h2>
-          <h1 className="text-white text-3xl font-bold sm:text-4xl mt-2">
+          <h1 className="text-white text-3xl sm:text-4xl lg:text-5xl mt-2">
             I'm a <span className="text-cyan-400">Full Stack Developer</span>
           </h1>
-          <p className="text-white mt-2 text-xl max-w-md">
+          <p className="text-white mt-2 text-lg sm:text-xl lg:text-2xl max-w-md mx-auto md:mx-0">
             Building Bridges Between Front-End and Back-End, Transforming Ideas into Web Reality
           </p>
 
-          <div>
-            <Link to="Project" 
-            smooth duration={500} 
-            className="group text-white w-fit px-6 py-3 mt-2 my-2 gap-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+          <div className="mt-4">
+            <Link
+              to="Project"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 mt-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
               Portfolio
-              <span className="group-hover:rotate-90 duration-300">
+              <span className="group-hover:rotate-90 duration-300 ml-2">
                 <MdArrowRightAlt />
               </span>
             </Link>
           </div>
-{/* 
-          <div className="">
-            <img src={hero} 
-              alt="my profile"
-              className="mt-4 rounded mx-auto w-2/3 md:w-full" />
-            </div> */}
+        </div>
+
+        {/* Hero Image */}
+        <div className="flex-shrink-0 w-full md:w-1/2 flex justify-center mt-4 md:mt-0">
+          <img src={hero} alt="my profile" className="rounded-2xl w-full max-w-sm md:max-w-md lg:max-w-lg" />
         </div>
       </div>
     </div>
   );
-}; 
+};
 
 export default Home;
